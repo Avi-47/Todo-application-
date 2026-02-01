1a0bca4ca5a6fbafcad90aca46c8de779f24b34f
Avimanyu Goswami <166274452+Avi-47@users.noreply.github.com>
2026-02-01 21:25:18 +0530

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reduxToolkitTodo/', // must match repo name
+  base: '/Todo-application-/', // ⚠ Must match your repo name exactly
 })
